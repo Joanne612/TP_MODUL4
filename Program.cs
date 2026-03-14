cb34e35f7e750ff4ff68b314feb245e6c340eb76
Joanne612 <joanneagnesian@gmail.com>
2026-03-14 18:48:24 +0700

    menambahkan class DoorMachine

--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
-﻿public class KodePos
+﻿public class Program
 {
     public static void Main(string[] args)
     {
-        System.Console.WriteLine("Masukkan nama kelurahan: ");
+        //KODE POS
+        Console.WriteLine("Masukkan nama kelurahan: ");
         string kel = Console.ReadLine();
 
         KodePos kp = new KodePos();
         int kode = kp.getKodePos(kel);
 
-        System.Console.WriteLine("Kode Pos: " + kode);
+        Console.WriteLine("Kode Pos: " + kode);
+
+        Console.WriteLine();
+
+        //DOOR MACHINE
+        DoorMachine.Main2();
     }
+}
+
+public class KodePos
+{
     public int getKodePos(string kelurahan)
     {
         int[] kodePos = {40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274};
@@ -23,5 +33,48 @@
             }
         }
         return 0;
+    }
+}
+
+public class  DoorMachine
+{
+    public enum State { TERKUNCI, TERBUKA }
+    public static void Main2()
+    {
+
+        State state = State.TERKUNCI;
+        if(state == State.TERKUNCI)
+        {
+            System.Console.WriteLine("Pintu terkunci");
+        }
+        else
+        {
+            System.Console.WriteLine("Pintu tidak terkunci");
+        }
+        Console.WriteLine(bukaPintu(state));
+        Console.WriteLine(kunciPintu(state));
+    }
+
+    public static string kunciPintu(State state)
+    {
+        if(state == State.TERKUNCI)
+        {
+            return "Pintu sudah terkunci";
+        }
+        else
+        {
+            return "Pintu terkunci";
+        }
+    }
+    public static string bukaPintu(State state)
+    {
+        if (state == State.TERKUNCI)
+        {
+            return "Pintu tidak terkunci";
+        }
+        else
+        {
+            return "Pintu sudah terbuka";
+        }
     }
 }
